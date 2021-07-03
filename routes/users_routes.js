26b64bd6c8043148999885d80889d33518e57ec1
jemblonganvalley <jemblonganvalley@gmail.com>
2021-07-04 00:45:01 +0800

    up

--- routes/users_routes.js
+++ routes/users_routes.js
@@ -40,4 +40,28 @@ users.post("/register-read", (req, res) => {
     });
 });
 
+users.post("/login", (req, res) => {
+  const data = req.body;
+  Users.findOne({ where: data, attributes: ["id", "username", "phone"] })
+    .then((result) => {
+      if (result) {
+        res.status(200).json({
+          success: true,
+          data: result,
+        });
+      } else {
+        res.status(404).json({
+          success: false,
+          msg: "check again your password",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(404).json({
+        success: false,
+        error: err,
+      });
+    });
+});
+
 module.exports = users;
